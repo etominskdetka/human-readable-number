f5c68f2e5ea9003e74b9ea4108183b656d2026e8
etominskdetka <9798dima@gmail.com>
2021-07-21 01:44:42 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function toReadable (number) {
+  if (typeof(number) !== 'number' || number < 0 || number > 1000) {
+    throw new Error('Enter a number between null and one thousand');
+  }
+
+  if (number === 0) {
+      return 'zero';
+  }
+
   
 }
