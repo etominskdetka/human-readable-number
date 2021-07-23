65ea1fbf1426a36424f9981365184be52dd5fbed
etominskdetka <9798dima@gmail.com>
2021-07-23 18:41:13 +0300

    feat: add required variables

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 module.exports = function toReadable (number) {
+
+  let str = '';
+
+  let num1 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven ','twelve ', 'thirteen  ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+
+  let num2 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  let num3 = 'hundred';
+
   if (typeof(number) !== 'number' || number < 0 || number > 1000) {
     throw new Error('Enter a number between null and one thousand');
   }
@@ -7,5 +16,4 @@ module.exports = function toReadable (number) {
       return 'zero';
   }
 
-  
 }
