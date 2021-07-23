bb538d6f870a26305851cdbab63a832a91e1c367
etominskdetka <9798dima@gmail.com>
2021-07-23 19:22:57 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,19 +1,47 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
+    let str = "";
 
-  let str = '';
+    let num1 = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven ",
+        "twelve ",
+        "thirteen  ",
+        "fourteen ",
+        "fifteen ",
+        "sixteen ",
+        "seventeen ",
+        "eighteen ",
+        "nineteen ",
+    ];
 
-  let num1 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven ','twelve ', 'thirteen  ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+    let num2 = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
 
-  let num2 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let num3 = "hundred";
 
-  let num3 = 'hundred';
+    if (typeof number !== "number" || number < 0 || number > 1000) {
+        throw new Error("Enter a number between null and one thousand");
+    }
 
-  if (typeof(number) !== 'number' || number < 0 || number > 1000) {
-    throw new Error('Enter a number between null and one thousand');
-  }
-
-  if (number === 0) {
-      return 'zero';
-  }
-
-}
+    if (number < 20) {
+        str = num1[number];
+    }
+};
