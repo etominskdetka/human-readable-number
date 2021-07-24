c98f93ac8a3e3924f9f8a09eace137cb5fb640f3
etominskdetka <9798dima@gmail.com>
2021-07-24 21:12:05 +0300

    feat: add function for number > 99

--- src/index.js
+++ src/index.js
@@ -56,5 +56,22 @@ module.exports = function toReadable(number) {
     }
 
     if (number < 1000) {
+        let hundRez = units[Math.floor(number / 100)];
+        let rez2;
+        if (number % 100 === 0) {
+            rez2 = "";
+        } else if (number % 100 < 20) {
+            rez2 = " " + units[number % 100];
+        } else if (number % 10 === 0 && number % 100 > 0) {
+            let doz3 = Math.floor(number / 100);
+            rez2 = " " + dozens[doz3];
+        } else {
+            rez2 =
+                " " +
+                dozens[Math.floor((number % 100) / 10)] +
+                " " +
+                units[number % 10];
+        }
+        return hundRez + " " + hundreds + rez2;
     }
 };
