5ac87b6ac64d8e61cad8438679371ef0b333506f
etominskdetka <9798dima@gmail.com>
2021-07-24 22:12:58 +0300

    fix: final tests

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ module.exports = function toReadable(number) {
 
     let dozens = [
         "",
-        "",
+        "ten",
         "twenty",
         "thirty",
         "forty",
@@ -58,14 +58,16 @@ module.exports = function toReadable(number) {
     if (number < 1000) {
         let hundRez = units[Math.floor(number / 100)];
         let rez2;
-        if (number % 100 === 0) {
+        if (number % 100 === 0) {                              // 100, 200, 300, 400
             rez2 = "";
-        } else if (number % 100 < 20) {
-            rez2 = " " + units[number % 100];
-        } else if (number % 10 === 0 && number % 100 > 0) {
-            let doz3 = Math.floor(number / 100);
-            rez2 = " " + dozens[doz3];
-        } else {
+
+        } else if (number % 10 === 0 /*&& number % 100 < 10*/) {  // 120, 960
+          rez2 = " " + dozens[Math.floor(number % 100) / 10];
+
+        } else if (number % 100 < 20) {                      // 112, 205
+          rez2 = " " + units[number % 100];
+
+        } else {                                             //963, 158
             rez2 =
                 " " +
                 dozens[Math.floor((number % 100) / 10)] +
